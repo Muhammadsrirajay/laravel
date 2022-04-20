2b9d3ec3ca2f707c0cdaa96f6d9bb9bd2fb15b6e
Muhammadsrirajay <Muhammadsrirajay@gmail.com>
2022-04-20 23:08:09 +0700

    menambahkan router baru

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Sederhana | About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Halaman About</h1>
+
+    <h3>Halo semua perkenalkan saya Muhammad Sri Rajay, Mahasiswa Universitas Semarang fakultas Teknik Informasi dan Komunikasi Progdi Teknik Informatika Kelas Sore</h3>
+    <img src="img/je.jpg" alt="Muhammad Sri Rajay" width="100">
+    
+</body>
+</html>
